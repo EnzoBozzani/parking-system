0f7963bb5d7185c2bbaf34a131d05905e40eef66
Enzo Bozzani <bozzanienzo@gmail.com>
2023-12-26 20:50:18 -0300

    feat: developed project

---
+++ Models/Estacionamento.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace desafio_estacionamento.Models
+{
+    public class Estacionamento
+    {
+        public Estacionamento()
+        {
+            Veiculos = new List<string>();
+        }
+        private List<string> Veiculos;
+
+        public void AdicionarVeiculo()
+        {
+            Console.WriteLine("Informe a placa do veículo: ");
+            string placa = Console.ReadLine();
+            Veiculos.Add(placa);
+            Console.WriteLine("Pressione ENTER para avançar");
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+        public void RemoverVeiculo(int initialRate, int hourRate)
+        {
+            if (Veiculos.Count == 0)
+            {
+                Console.WriteLine("Estacionamento vazio.");
+                Console.WriteLine("Pressione ENTER para avançar");
+                Console.ReadLine();
+                Console.Clear();
+                return;
+
+            }
+            Console.WriteLine("Informe a placa do veículo: ");
+            string placa = Console.ReadLine();
+            Console.WriteLine("Informe quantas horas o veículo ficou: ");
+            int.TryParse(Console.ReadLine(), out int hours);
+            int tax = initialRate + (hours * hourRate);
+            Veiculos.Remove(placa);
+            Console.WriteLine($"O veículod de placa {placa} foi removido e o valor é de {tax:C}");
+            Console.WriteLine("Pressione ENTER para avançar");
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+        public void ListarVeiculos()
+        {
+            if (Veiculos.Count == 0)
+            {
+                Console.WriteLine("Estacionamento vazio.");
+                Console.WriteLine("Pressione ENTER para avançar");
+                Console.ReadLine();
+                Console.Clear();
+                return;
+
+            }
+            for (int i = 0; i < Veiculos.Count; i++)
+            {
+                Console.WriteLine($"Veiculo {i}: {Veiculos[i]}");
+            }
+            Console.WriteLine("Pressione ENTER para avançar");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using desafio_estacionamento.Models;
+
+Estacionamento estacionamento = new Estacionamento();
+Console.WriteLine("Seja bem vindo ao estacionamento! ");
+Console.WriteLine("Digite a taxa inicial: ");
+bool success = int.TryParse(Console.ReadLine(), out int initialRate);
+while (!success)
+{
+    Console.WriteLine("VALOR INVÁLIDO!\nDigite o preço inicial: ");
+    success = int.TryParse(Console.ReadLine(), out initialRate);
+}
+
+Console.WriteLine("\nDigite a taxa por hora: ");
+success = int.TryParse(Console.ReadLine(), out int hourRate);
+while (!success)
+{
+    Console.WriteLine("VALOR INVÁLIDO!\nDigite a taxa por hora: ");
+    success = int.TryParse(Console.ReadLine(), out hourRate);
+}
+
+string option = "";
+
+while (option != "4")
+{
+    Console.WriteLine("Digite a sua opção: ");
+    Console.WriteLine("1 - Cadastrar Veículo");
+    Console.WriteLine("2 - Remover Veículo");
+    Console.WriteLine("3 - Listar Veículos");
+    Console.WriteLine("4 - Encerrar");
+    option = Console.ReadLine();
+    switch (option)
+    {
+        case "1":
+            estacionamento.AdicionarVeiculo();
+            break;
+        case "2":
+            estacionamento.RemoverVeiculo(initialRate, hourRate);
+            break;
+        case "3":
+            estacionamento.ListarVeiculos();
+            break;
+        case "4":
+            Console.WriteLine("Obrigado por utilizar!");
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine("Digite uma opcão válida!");
+            break;
+    }
+}
